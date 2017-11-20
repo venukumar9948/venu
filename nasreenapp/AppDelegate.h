fc4a33822051f47ae40b4796707bf1ac30fd1112
student <student@achiever-87.local>
2017-11-20 17:40:38 +0530

    Initial Commit

---
+++ nasreenapp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  nasreenapp
+//
+//  Created by student on 11/17/17.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ nasreenapp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  nasreenapp
+//
+//  Created by student on 11/17/17.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
